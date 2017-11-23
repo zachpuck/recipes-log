9f847c2d279ae6be9b2a26896c4d6099585249e4
Shelly Tang <shellytang@gmail.com>
2017-11-23 00:09:55 -0800

    basic tests and setup for user route

--- model/user-model.js
+++ model/user-model.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String, required: true, lowercase: true },
-  email: {type: String, required: true, lowercase: true},
+  username: { type: String, required: true, lowercase: true, unique: true},
+  email: {type: String, required: true, lowercase: true, unique: true},
   // password: {type: String, required: true},
 });
 
--- route/auth-route.js
+++ route/auth-route.js
@@ -4,9 +4,10 @@ const User = require('../model/user-model');
 
 module.exports = router => {
   router.post('/signup', (req, res, next) => {
+    console.log('what', req.body.email);
     return new User(req.body)
       .save()
-      .then(user =>  res.json(user))
+      .then(user => res.json(user))
       .catch(next);
   });
 
